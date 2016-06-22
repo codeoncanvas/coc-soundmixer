c98ce590279865f7a02ff345b2289799b1df3e1c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2016-06-22 14:55:02 +1000

    licensing information
    add license file and notices in source

--- src/cocSoundMixer.h
+++ src/cocSoundMixer.h
@@ -1,10 +1,21 @@
-//
-//  SoundMixer.h
-//  emptyExample
-//
-//  Created by Lukasz Karluk on 6/11/12.
-//
-//
+/**
+ *
+ *      ┌─┐╔═╗┌┬┐┌─┐
+ *      │  ║ ║ ││├┤
+ *      └─┘╚═╝─┴┘└─┘
+ *   ┌─┐┌─┐╔╗╔┬  ┬┌─┐┌─┐
+ *   │  ├─┤║║║└┐┌┘├─┤└─┐
+ *   └─┘┴ ┴╝╚╝ └┘ ┴ ┴└─┘
+ *
+ * Copyright (c) 2012-2016 Code on Canvas Pty Ltd, http://CodeOnCanvas.cc
+ *
+ * This software is distributed under the MIT license
+ * https://tldrlegal.com/license/mit-license
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code
+ *
+ **/
 
 #pragma once
 
@@ -32,37 +43,37 @@ class SoundItem {
 public:
 
     SoundItem() {
-        
+
         soundPath = "";
         soundID = "";
-        
+
         bLoaded = false;
         bPlay = false;
         bPause = false;
         bLoop = false;
-        
+
         timeCurrent = 0;
         timeDuration = 0;
         progress = 0;
 
         numOfTimesPlayed = 0;
         numOfTimesToPlay = 1;
-        
+
         volume = 1.0;
         volumeShape.push_back(SoundPoint(volume));
-        
+
         panning = 0.5;
         panningShape.push_back(SoundPoint(panning));
     }
 
     std::string soundPath;
     std::string soundID;
-    
+
     Value<bool> bLoaded;
     Value<bool> bPlay;
     Value<bool> bPause;
     Value<bool> bLoop;
-    
+
     Value<float> timeCurrent;
     Value<float> timeDuration;
     Value<float> progress;
@@ -72,47 +83,47 @@ public:
 
     Value<float> volume;
     std::vector<SoundPoint> volumeShape;
-    
+
     Value<float> panning;
     std::vector<SoundPoint> panningShape;
 };
 
 //--------------------------------------------------------------
 class SoundMixer {
-    
+
 public:
-    
+
     SoundMixer();
     ~SoundMixer();
-    
+
     virtual const SoundItem & addSound(std::string soundPath, std::string soundID="");
     virtual void removeSound(std::string soundID);
-    
+
     virtual void load(std::string soundID);
     virtual void unload(std::string soundID);
-    
+
     void setMasterVolume(float value);
     void setMasterPanning(float value);
-    
+
     virtual void setVolume(std::string soundID, float value);
     virtual void setPanning(std::string soundID, float value);
     virtual void setNumOfPlays(std::string soundID, int numOfPlays);
-    
+
     virtual void play(std::string soundID);
     virtual void stop(std::string soundID);
     virtual void pause(std::string soundID);
-    
+
     virtual void update(float timeDelta=0);
-    
+
     const SoundItem * getSound(std::string soundID);
-    
+
 protected:
 
     virtual SoundItem * initSound();
     virtual void killSound(SoundItem * sound);
-    
+
     SoundItem * getSoundPtr(std::string soundID);
-    
+
     Value<float> masterVolume;
     Value<float> masterPanning;
     std::vector<SoundItem *> sounds;
